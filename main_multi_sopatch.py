05fb5aa1e4113487d537526e92efd2e8c8addf10
Bend1031 <czy1145554053@gmail.com>
2023-11-27 16:03:37 +0800

    Refactor evaluation metrics and add data analysis

--- main_multi_sopatch.py
+++ main_multi_sopatch.py
@@ -20,9 +20,8 @@ from components import load_component
 
 # from lib.config import Config
 from lib.rootpath import rootPath
-from lib.utils import pix2pix_RMSE
-
-from .utils.evaluation_utils import estimate_homo
+from lib.utils import corr_MMA, pix2pix_RMSE
+from utils.evaluation_utils import estimate_homo
 
 
 @hydra.main(
@@ -34,9 +33,7 @@ from .utils.evaluation_utils import estimate_homo
 def main(config):
     log = logging.getLogger(__name__)
     # 读取图片路径
-
     method = config.method
-
     dataset = config.dataset.dataset_path
     imgfiles1 = glob.glob(str(rootPath / dataset) + r"/test/opt/*.png")
     imgfiles2 = glob.glob(str(rootPath / dataset) + r"/test/sar/*.png")
@@ -46,13 +43,29 @@ def main(config):
     matcher = load_component("matcher", method.matcher.name, method.matcher)
     ransac = load_component("ransac", method.ransac.name, method.ransac)
 
+    # %% 评价指标
+    # 平均提取点数
+    mean_extract_nums = []
+    # 平均匹配点数
+    mean_match_nums = []
+    # 去除离散点后的匹配点数
+    mean_ransac_nums = []
+    # 匹配后mma
+    m_corr_mma = {}
+    m_ransac_mma = {}
+    # 利用角点进行单应性矩阵精度的评估，方式同MMA
+    # m_dists_homo_mma = {}
+    dists_homo = []
+    # 失败情况
+    failed_nums = 0
+
+    # 阈值3情况下的指标
     mRMSE = []
     mNCM = []
     mCMR = []
-    mERR = {}
     success_num = 0
-    h_failed = 0
-    dists_homo = []
+
+    # %%
     start_time = time.perf_counter()
 
     for i in tqdm(range(len(imgfiles1))):
@@ -68,9 +81,12 @@ def main(config):
         # %% extractor提取特征点和描述子
         kpt1, desc1 = extractor.run(img1_path)
         kpt2, desc2 = extractor.run(img2_path)
-        if len(kpt1) <= 4 or len(kpt2) <= 4:
+
+        if len(kpt1) == 0 or len(kpt2) == 0:
+            failed_nums += 1
             continue
 
+        mean_extract_nums.append(np.mean([len(kpt1), len(kpt2)], dtype=int))
         # %% matcher
         test_data = {
             "x1": kpt1,
@@ -82,48 +98,69 @@ def main(config):
         }
         corr1, corr2 = matcher.run(test_data)
         if len(corr1) <= 4 or len(corr2) <= 4:
+            failed_nums += 1
             continue
-
+        mean_match_nums.append(np.mean([len(corr1), len(corr2)], dtype=int))
+        corr_mma = corr_MMA(corr1, corr2)
+        for key in corr_mma:
+            m_corr_mma[key] = m_corr_mma.get(key, 0) + corr_mma[key]
         # %% ransac
         H_pred, corr1, corr2 = ransac.run(corr1, corr2)
-        if len(corr1) <= 4 or len(corr2) <= 4:
+        if len(corr1) <= 4 or len(corr2) <= 4 or H_pred is None:
+            failed_nums += 1
             continue
-        # %%evaluation homography estimation
+        mean_ransac_nums.append(np.mean([len(corr1), len(corr2)], dtype=int))
+        # %%evaluation homography estimation:list
         corner_dist = estimate_homo(img1, H_pred)
         dists_homo.append(corner_dist)
         # %%evaluation
-
-        RMSE, NCM, CMR, bool_list, err = pix2pix_RMSE(corr1, corr2)
+        RMSE, NCM, CMR, bool_list, ransac_mma = pix2pix_RMSE(corr1, corr2)
         if NCM >= 5:
             success_num += 1
             mNCM.append(NCM)
             mCMR.append(CMR)
             mRMSE.append(RMSE)
-            for key in err:
-                mERR[key] = mERR.get(key, 0) + err[key]
+            for key in ransac_mma:
+                m_ransac_mma[key] = m_ransac_mma.get(key, 0) + ransac_mma[key]
+        else:
+            failed_nums += 1
 
-    thres = [1, 3, 5, 10]
-
+    # %%数据集精度评估
+    mean_extract_nums = int(np.mean(mean_extract_nums))
+    # 平均匹配点数
+    mean_match_nums = int(np.mean(mean_match_nums))
+    # 去除离散点后的匹配点数
+    mean_ransac_nums = int(np.mean(mean_ransac_nums))
+    # homo
+    thresholds = list(range(1, 11))
     homo_acc = np.mean(
-        [[float(dist <= t) for t in thres] for dist in dists_homo], axis=0
+        [[float(dist <= t) for t in thresholds] for dist in dists_homo], axis=0
     )
-    homo_acc = {t: acc for t, acc in zip(thres, homo_acc)}
-    homo_acc = round(homo_acc, 3)
-    print(homo_acc)
+    m_homo_mma = {t: round(acc, 3) for t, acc in zip(thresholds, homo_acc)}
+    # print(homo_acc)
 
-    for key in mERR:
-        mERR[key] /= success_num
+    for key in m_corr_mma:
+        m_corr_mma[key] = round(m_corr_mma[key] / success_num, 3)
+    for key in m_ransac_mma:
+        m_ransac_mma[key] = round(m_ransac_mma[key] / success_num, 3)
 
     end_time = time.perf_counter()
 
-    # %% 数据分析
-    # save err
-    err = {
+    # %% 数据分析，保存为 json
+    # save data
+    data = {
         "method": f"{method.name}",
         "dataset": f"{config.dataset.name}",
-        "MMA": mERR,
-        "homo_acc": homo_acc,
-        "success_rate": success_num / len(imgfiles1),
+        "dataset_size": len(imgfiles1),
+        "success_num": success_num,
+        "failed_nums": failed_nums,
+        "success_rate": round(success_num / len(imgfiles1), 3),
+        "mean_extract_nums": mean_extract_nums,
+        "mean_match_nums": mean_match_nums,
+        "mean_ransac_nums": mean_ransac_nums,
+        "corr_mma": m_corr_mma,
+        "ransac_mma": m_ransac_mma,
+        "homo_mma": m_homo_mma,
         "NCM": f"{np.mean(mNCM):.1f}",
         "CMR": f"{np.mean(mCMR):.2f}",
         "RMSE": f"{np.mean(mRMSE):.2f}",
@@ -133,13 +170,20 @@ def main(config):
         f"result/{method.name}_{config.dataset.name}.json",
         "w",
     ) as f:
-        json.dump(err, f)
+        json.dump(data, f)
 
     log.info(f"Method:{method.name}")
     log.info(f"Dataset:{config.dataset.name}")
     log.info(f"Dataset size:{len(imgfiles1)}")
     log.info(f"Success num:{success_num}")
+    log.info(f"failed_nums: {failed_nums}")
     log.info(f"Success rate:{success_num / len(imgfiles1):.3f}")
+    log.info(f"mean_extract_nums: {mean_extract_nums}")
+    log.info(f"mean_match_nums: {mean_match_nums}")
+    log.info(f"mean_ransac_nums: {mean_ransac_nums}")
+    log.info(f"corr_mma: {m_corr_mma}")
+    log.info(f"ransac_mma: {m_ransac_mma}")
+    log.info(f"homo_mma: {m_homo_mma}")
     log.info(f"NCM:{np.mean(mNCM):.1f}")
     log.info(f"CMR:{np.mean(mCMR):.2f}")
     log.info(f"RMSE:{np.mean(mRMSE):.2f}")
@@ -149,3 +193,5 @@ def main(config):
 
 if __name__ == "__main__":
     main()
+
+# %%
